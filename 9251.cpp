037179092d2e7292647c8cbd908c2bbe20547a9e
Seongjin Cho <139206420+5j1n@users.noreply.github.com>
2025-01-07 13:33:20 +0900

    Create 9251.cpp

---
+++ 9251.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1001][1001];
+int main() {
+	string str1, str2;
+	cin >> str1 >> str2;
+	int n = str1.length();
+	int m = str2.length();
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (str1[i-1] == str2[j-1])
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+	cout << dp[n][m];
+}
