c70e3d33655283c9a9d28a240be2f5d6d21b63b3
Seongjin Cho <139206420+5j1n@users.noreply.github.com>
2025-01-09 20:15:35 +0900

    Create 11060.cpp

---
+++ 11060.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+long long arr[1001], dp[1001];
+
+int main() {
+	long long n;
+	cin >> n;
+	for (long long i = 1; i <= n; i++) {
+		cin >> arr[i];
+		dp[i] = INT_MAX;
+	}
+
+	dp[1] = 0;
+
+	for (long long i = 1; i < n; i++) {
+		for (long long j = i + 1; j <= min(i + arr[i], n); j++) {
+			dp[j] = min(dp[j], dp[i] + 1);
+		}
+	}
+	if (dp[n] == INT_MAX) cout << -1;
+	else
+		cout << dp[n];
+}
