c6ae0085d76b9a0b4d8e71f93c774e06cc9439b8
Seongjin Cho <139206420+5j1n@users.noreply.github.com>
2025-01-10 12:50:29 +0900

    Create 11651.cpp

---
+++ 11651.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool compare(pair<int, int>a, pair<int, int>b) {
+	if (a.second == b.second)
+		return a.first < b.first;
+	return a.second < b.second;
+}
+int main() {
+	int n;
+	cin >> n;
+	vector<pair<int, int>>v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i].first >> v[i].second;
+	}
+	sort(v.begin(), v.end(), compare);
+	for (int i = 0; i < n; i++) {
+		cout<< v[i].first <<" "<< v[i].second<<"\n";
+	}
+}
