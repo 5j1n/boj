d44a2808c75c73c94efb2bf143c4d98c19ffa841
Seongjin Cho <139206420+5j1n@users.noreply.github.com>
2025-01-15 14:57:53 +0900

    Create 15657.cpp

---
+++ 15657.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m;
+vector<int>v;
+bool used[10001];
+int arr[9];
+
+void tracking(int size) {
+	if (size == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		if (arr[size - 1] <= v[i]) {
+			arr[size] = v[i];
+			tracking(size + 1);
+		}
+	}
+
+}
+int main() {
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	sort(v.begin(), v.end());
+
+	tracking(0);
+		
+}
